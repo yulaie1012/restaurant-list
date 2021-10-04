0353189c762c324de0d5f3251f0d1ab26137a00a
Luo, Shi-Wei <yulaie1012@gmail.com>
2021-10-04 17:50:39 +0800

    refactor: handlebars is abbreviated to 'hbs'

--- app.js
+++ app.js
@@ -19,8 +19,8 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
-app.set('view engine', 'handlebars')
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
 
 app.use(express.static('public'))
 
