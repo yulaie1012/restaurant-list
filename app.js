26add292ad974394ffb470a9a1632ed49d7537c6
Luo, Shi-Wei <yulaie1012@gmail.com>
2021-10-26 17:23:30 +0800

    feat: add isAuthenticated to display different view templates

--- app.js
+++ app.js
@@ -24,6 +24,13 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
 app.use(methodOverride('_method'))
 usePassport(app)
+
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  next()
+})
+
 app.use(routes)
 
 app.listen(port, () => {
