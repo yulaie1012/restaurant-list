5465a205aba9770d9e756502fe720f7871dd6c50
Luo, Shi-Wei <yulaie1012@gmail.com>
2021-10-26 16:51:41 +0800

    feat: add user register feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
 
+const User = require('../../models/user')
+
 router.get('/login', (req, res) => {
   res.render('login')
 })
@@ -11,6 +13,21 @@ router.post('/login', (req, res) => {
 
 router.get('/register', (req, res) => {
   res.render('register')
+})
+
+router.post('/register', (req, res) => {
+  const { name, email, password, confirmPassowrd } = req.body
+
+  User.findOne({ email })
+    .then((user) => {
+      if (user) {
+        console.log('User already exists.')
+        return res.render('register', { name, email, password, confirmPassowrd })
+      }
+      return User.create({ name, email, password })
+        .then(() => res.redirect('/'))
+        .catch(err => console.log(err))
+    })
 })
 
 module.exports = router
