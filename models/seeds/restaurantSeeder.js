8159bf540f9b1bede08f6939c9b12488b48f7903
Luo, Shi-Wei <yulaie1012@gmail.com>
2021-10-26 16:34:00 +0800

    feat: add edit and show buttons on index page

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -4,17 +4,7 @@ const restaurantData = require('./restaurant.json').results
 
 db.once('open', () => {
   restaurantData.forEach(item => {
-    Restaurant.create({
-      name: item.name,
-      name_en: item.name_en,
-      category: item.category,
-      image: item.image,
-      location: item.location,
-      phone: item.phone,
-      google_map: item.google_map,
-      rating: item.rating,
-      description: item.description
-    })
+    Restaurant.create(item)
   })
   console.log('done')
 })
